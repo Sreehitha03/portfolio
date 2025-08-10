dc94eee8dbbe17081d374ddbf9afcddd4289a58e
SreehithaK <152504909+Sreehitha03@users.noreply.github.com>
2025-08-10 19:18:56 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,32 @@
-import {motion} from 'framer-motion';
+import { useEffect, useState } from 'react';
+import { motion } from 'framer-motion';
 import { styles } from '../styles';
 import { ComputersCanvas } from './canvas';
 
-
 const Hero = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(max-width: 500px)');
+    setIsMobile(mediaQuery.matches);
+
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    mediaQuery.addEventListener('change', handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange);
+    };
+  }, []);
+
   return (
     <section className='relative w-full h-screen mx-auto'>
       <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
         <div className='flex flex-col justify-center items-center mt-5'>
-          <div className='w-5 h-5 rounded-full bg-[#915eff]'/>
-          <div className='w-1 sm:h-80 h-40 violet-gradient'/>
+          <div className='w-5 h-5 rounded-full bg-[#915eff]' />
+          <div className='w-1 sm:h-80 h-40 violet-gradient' />
         </div>
 
         <div>
@@ -20,13 +37,13 @@ const Hero = () => {
           </p>
         </div>
       </div>
-      <ComputersCanvas/>
-      <div className='absolute xs:bottom-10 bottom-32
-      w-full flex justify-center items-center'>
+
+      {isMobile ? null : <ComputersCanvas />}
+
+      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href="#about">
-          <div className='w-[30px] h-[45px] rounded-3xl border-4 border-secondary 
-          flex justify-center items-start p-2'>
-            <motion.div 
+          <div className='w-[30px] h-[45px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+            <motion.div
               animate={{ y: [0, 18, 0] }}
               transition={{ duration: 1.5, repeat: Infinity, repeatType: 'loop' }}
               className='w-2 h-2 rounded-full bg-secondary mb-1'
@@ -34,9 +51,8 @@ const Hero = () => {
           </div>
         </a>
       </div>
-
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
